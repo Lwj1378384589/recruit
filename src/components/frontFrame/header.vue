8ebf47e15b17b281f1c40cc5876b07ae5185c3cb
Lwj1378384589 <1378384589@qq.com>
2018-04-04 10:00:37 +0800

    header

--- src/components/frontFrame/header.vue
+++ src/components/frontFrame/header.vue
@@ -1,12 +1,39 @@
 <template>
+    <div>
+        <div id="wrap">
+                <div class="container1">
+                    <ul class="css-left">
+                        <li class="shouye">
+                            <router-link to="/frontPage/index">网站首页</router-link>
+                            <!-- <a href="index.html"></a> -->
+                        </li>
+                    </ul>
+                    <ul class="css-right">
+                        <li>
+                                <router-link to="/frontPage/disLogin"> 登录 </router-link>
+                            <!-- <a href="disLogin.html"  class="log">登录</a> -->
+                        </li>
+                        <li>
+                            <router-link to="/frontPage/distRegist">免费注册 </router-link>
+                            <!-- <a href="distRegist.html"  class="regs">免费注册</a> -->
+                        </li>
+                        <li>
+                            <router-link to="/frontPage/help">帮助中心 </router-link>
+                            <!-- <a href="help.html"  class="hlp">帮助中心</a> -->
+                        </li>
+                    </ul>
+                </div>
+            </div>
         <div  style="padding-top:40px;"><!-- class="css-out-content" -->
             <div class="css-header"  style="font-size:16px;">
                 <div class="container">
-                    <a href="index.html" title="惠安智慧就业平台">
+                    <router-link to="/frontPage/index" title="惠安智慧就业平台"> </router-link>
+                    <!-- <a href="index.html" title="惠安智慧就业平台"> -->
                         <img src="imgs/a.jpg" class="css-logo pull-left" style="width:230px; height:40px;" >
                     </a>
                     <div class="css-subwebsite pull-left"  style="background: url('imgs/enterr.png') no-repeat left 5px;">
-                        <a href="SchoolList.html"><b>进入子站</b><span class="text-primary">[切换分站]</span></a>
+                        <router-link to="/frontPage/SchoolList"><b>进入子站</b><span class="text-primary">[切换分站]</span> </router-link>
+                        <!-- <a href="SchoolList.html"><b>进入子站</b><span class="text-primary">[切换分站]</span></a> -->
                     </div>
                     <div class="css-right pull-right">
                         <div class="css-search pull-right">
@@ -25,11 +52,22 @@
                     </div>
                 </div>
             </div>
-
-
-
-            
         </div>
+
+        <div class="css-nav">
+                <div class="container">
+                    <div class="pull-left css-usernav">
+                        <span onmouseover="seen()" onmouseout="disn()">用户功能频道</span>
+                    </div>
+                    <ul class="pull-left css-navbar">
+                        <li><a href="index.html">平台首页</a></li>
+                        <li><a href="StudentLogin.html">学生用户</a></li>
+                    </ul>
+                </div>
+        </div>
+        
+      
+    </div>
 </template>
 <script>
 
