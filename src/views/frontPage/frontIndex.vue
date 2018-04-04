dd560ca3477678677e481e78a1351ec52db47f90
liuruifeng <402788946@qq.com>
2018-04-04 10:05:34 +0800

    fontIndex

--- src/views/frontPage/frontIndex.vue
+++ src/views/frontPage/frontIndex.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>test</div>
+    <div id='frontIndex'>
+        <p>test</p>
+        <br/>
 
-    
+    </div>
 </template>
+<script>
+
+
+</script>
+
+<style>
+#frontIndex{
+    background:blueviolet;
+    width:800px;
+    height:800px;
+
+}
+
+</style>
