0a37dc394e580c8f8ed4124908e876258e3204e4
liuruifeng <402788946@qq.com>
2018-04-23 09:49:16 +0800

    后台侧菜单
    Signed-off-by: liuruifeng <402788946@qq.com>

--- src/router/routes.js
+++ src/router/routes.js
@@ -47,12 +47,6 @@ const _routes=[
             },
            ...disRoutes,
             
-            {
-                path:'/frontPage/testPage',
-                component:testPage,
-                name:'testPage'
-            },
-            
 
         ]
 
